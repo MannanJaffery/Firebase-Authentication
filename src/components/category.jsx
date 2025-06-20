a07d77321c50187b571435088ef969946ca37caf
Abdul Mannan <abdulmannan5497@gmail.com>
2025-06-20 16:20:39 +0500

    main

--- src/components/category.jsx
+++ src/components/category.jsx
@@ -37,7 +37,7 @@ const Category = () => {
 
   return (
 
-<div className="min-h-screen flex flex-col bg-gray-50">
+<div className="min-h-screen flex flex-col bg-gray-50 bg-grid-pattern bg-grid ">
 
   <header className="shadow bg-white">
     <Navbar />
